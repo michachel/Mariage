65a0101f663a1f2ff30a0d83348cbcb664e8877b
michachel <mickael_coulombel@yahoo.fr>
2015-11-17 01:15:43 +0100

    debug version en ligne + ajout utilisateur SQL + correction problèmes mineurs

--- Mariage/Models/etape.cs
+++ Mariage/Models/etape.cs
@@ -11,8 +11,7 @@ namespace Mariage.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class etape
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
